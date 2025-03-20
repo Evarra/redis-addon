271b49122d67af5391096e99721cdf7a4cc3a9c7
Evarra <47775538+Evarra@users.noreply.github.com>
2025-03-20 22:46:03 +0200

    Add files via upload

--- redis/run.sh
+++ redis/run.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Starting My Example Add-on"
-echo "Greeting: ${GREETING}"
+DATA_DIR=/addon_configs/612aff27_redis_persistent
+CONF_FILE="${DATA_DIR}/redis.conf"
 
-# Example of a long-running process:
-while true; do
-    echo "$(date): ${GREETING}"
-    sleep 30
-done
+mkdir -p "${DATA_DIR}"
 
+if [ ! -f "${CONF_FILE}" ]; then
+  echo "redis.conf ei eksisteeri, loon vaikimisi faili"
+  cat <<EOL > "${CONF_FILE}"
+bind 0.0.0.0
+port 6379
+appendonly yes
+appendfilename "appendonly.aof"
+dir ${DATA_DIR}
+save 900 1
+save 300 10
+save 60 10000
+loglevel notice
+protected-mode no
+EOL
+fi
+
+echo "Redis start käivitamine conf-failiga ${CONF_FILE}"
+
+exec redis-server "${CONF_FILE}"
