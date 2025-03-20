111002b867a33879e06841dae117279c2ceeb0d8
Evarra <47775538+Evarra@users.noreply.github.com>
2025-03-20 23:07:21 +0200

    Add files via upload

--- redis/run.sh
+++ redis/run.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-# Väline konfiguratsiooni asukoht
-EXTERNAL_CONF_DIR="/share/redis_persistent"
-EXTERNAL_CONF_FILE="${EXTERNAL_CONF_DIR}/redis.conf"
+# Väline kataloog, kuhu Redis salvestab andmed ja konfiguratsioonifailid
+PERSISTENT_DIR="/share/redis_persistent"
+CONF_FILE="${PERSISTENT_DIR}/redis.conf"
 
-# Sisemine andmete asukoht
-DATA_DIR="/addon_configs/612aff27_redis_persistent"
+mkdir -p "${PERSISTENT_DIR}"
 
-mkdir -p "${DATA_DIR}"
-mkdir -p "${EXTERNAL_CONF_DIR}"
-
-# Kontrolli, kas väline konfiguratsioonifail eksisteerib, kui ei, loo vaikimisi
-if [ ! -f "${EXTERNAL_CONF_FILE}" ]; then
-  echo "redis.conf ei eksisteeri, loon vaikimisi faili ${EXTERNAL_CONF_FILE}"
-  cat <<EOL > "${EXTERNAL_CONF_FILE}"
+# Kui redis.conf ei eksisteeri, loo see vaikimisi seadetega
+if [ ! -f "${CONF_FILE}" ]; then
+  echo "redis.conf ei eksisteeri, loon vaikimisi faili ${CONF_FILE}"
+  cat <<EOL > "${CONF_FILE}"
 bind 0.0.0.0
 port 6379
 appendonly yes
 appendfilename "appendonly.aof"
-dir ${DATA_DIR}
+dbfilename "dump.rdb"
+dir ${PERSISTENT_DIR}  # Redis-i andmefailid salvestatakse siia
 save 900 1
 save 300 10
 save 60 10000
@@ -28,22 +25,21 @@ protected-mode no
 EOL
 fi
 
-# Lisa võimalus lugeda Home Assistant add-on valikuid
+# Kui Home Assistant add-on konfiguratsioon sisaldab seadistusi, lisa need
 if [ -n "$SUPERVISOR_TOKEN" ]; then
     HA_OPTIONS=$(curl -s -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/options)
     REDIS_PASSWORD=$(echo "$HA_OPTIONS" | jq -r '.requirepass // empty')
     MAX_MEMORY_POLICY=$(echo "$HA_OPTIONS" | jq -r '.maxmemory_policy // empty')
 
     if [ -n "$REDIS_PASSWORD" ]; then
-        echo "requirepass ${REDIS_PASSWORD}" >> "${EXTERNAL_CONF_FILE}"
+        echo "requirepass ${REDIS_PASSWORD}" >> "${CONF_FILE}"
     fi
 
     if [ -n "$MAX_MEMORY_POLICY" ]; then
-        echo "maxmemory-policy ${MAX_MEMORY_POLICY}" >> "${EXTERNAL_CONF_FILE}"
+        echo "maxmemory-policy ${MAX_MEMORY_POLICY}" >> "${CONF_FILE}"
     fi
 fi
 
-echo "Redis start käivitamine conf-failiga ${EXTERNAL_CONF_FILE}"
+echo "Redis käivitamine conf-failiga ${CONF_FILE}"
 
-exec redis-server "${EXTERNAL_CONF_FILE}"
-
+exec redis-server "${CONF_FILE}"
